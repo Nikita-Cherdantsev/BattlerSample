d3ebb35d15899bde4610e02d4a587f3ab5863e7b
Eugenia Kolina <kolinaea@gmail.com>
2025-10-10 17:04:19 +0400

    [ Manifest ] roblox assets ids updated.

--- src/shared/Modules/Assets/Manifest.lua
+++ src/shared/Modules/Assets/Manifest.lua
@@ -10,30 +10,30 @@ local Manifest = {}
 -- Card images (TODO: Replace with actual card art)
 Manifest.CardImages = {
 	-- Legendary cards
-	["card_100"] = "rbxassetid://0",  -- Monkey D. Luffy
-	["card_200"] = "rbxassetid://0",  -- Roronoa Zoro
-	["card_800"] = "rbxassetid://0",  -- Vegeta
-	["card_1000"] = "rbxassetid://0", -- Goku
+	["card_100"] = "rbxassetid://78423585996740",  -- Monkey D. Luffy
+	["card_200"] = "rbxassetid://129594750129124",  -- Roronoa Zoro
+	["card_800"] = "rbxassetid://95838232303458",  -- Vegeta
+	["card_1000"] = "rbxassetid://95259815635405", -- Goku
 	
 	-- Epic cards
-	["card_300"] = "rbxassetid://0",  -- Rock Lee
-	["card_400"] = "rbxassetid://0",  -- Tsunade
-	["card_1200"] = "rbxassetid://0", -- All Might
+	["card_300"] = "rbxassetid://109391071241644",  -- Rock Lee
+	["card_400"] = "rbxassetid://129627997182384",  -- Tsunade
+	["card_1200"] = "rbxassetid://129062570319296", -- All Might
 	
 	-- Rare cards
-	["card_500"] = "rbxassetid://0",  -- Sanji
-	["card_900"] = "rbxassetid://0",  -- Shino Aburame
-	["card_1500"] = "rbxassetid://0", -- Bakugo
+	["card_500"] = "rbxassetid://114047411638621",  -- Sanji
+	["card_900"] = "rbxassetid://107307003564755",  -- Shino Aburame
+	["card_1500"] = "rbxassetid://105914633790557", -- Bakugo
 	
 	-- Uncommon cards
-	["card_600"] = "rbxassetid://0",  -- Tenten
-	["card_700"] = "rbxassetid://0",  -- Koby
-	["card_1100"] = "rbxassetid://0", -- Usopp
-	["card_1300"] = "rbxassetid://0", -- Chopper
-	["card_1400"] = "rbxassetid://0", -- Krillin
-	["card_1600"] = "rbxassetid://0", -- Yamcha
-	["card_1700"] = "rbxassetid://0", -- Midoriya
-	["card_1800"] = "rbxassetid://0", -- Piccolo
+	["card_600"] = "rbxassetid://117487006318467",  -- Tenten
+	["card_700"] = "rbxassetid://123184517185097",  -- Koby
+	["card_1100"] = "rbxassetid://102339751039385", -- Usopp
+	["card_1300"] = "rbxassetid://114677338404508", -- Chopper
+	["card_1400"] = "rbxassetid://76533453851792", -- Krillin
+	["card_1600"] = "rbxassetid://119952205768312", -- Yamcha
+	["card_1700"] = "rbxassetid://125875363703877", -- Midoriya
+	["card_1800"] = "rbxassetid://82447764673886", -- Piccolo
 }
 
 -- Card images (TODO: Replace with actual card art)
@@ -45,24 +45,24 @@ Manifest.CardImagesDisabled = {
 	["card_1000"] = "rbxassetid://101835154423145", -- Goku
 	
 	-- Epic cards
-	["card_300"] = "rbxassetid://0",  -- Rock Lee
-	["card_400"] = "rbxassetid://0",  -- Tsunade
-	["card_1200"] = "rbxassetid://0", -- All Might
+	["card_300"] = "rbxassetid://97638914176130",  -- Rock Lee
+	["card_400"] = "rbxassetid://71898620972133",  -- Tsunade
+	["card_1200"] = "rbxassetid://82108859330046", -- All Might
 	
 	-- Rare cards
-	["card_500"] = "rbxassetid://0",  -- Sanji
-	["card_900"] = "rbxassetid://0",  -- Shino Aburame
-	["card_1500"] = "rbxassetid://0", -- Bakugo
+	["card_500"] = "rbxassetid://140547037001157",  -- Sanji
+	["card_900"] = "rbxassetid://131459926213865",  -- Shino Aburame
+	["card_1500"] = "rbxassetid://120103825726197", -- Bakugo
 	
 	-- Uncommon cards
-	["card_600"] = "rbxassetid://0",  -- Tenten
-	["card_700"] = "rbxassetid://0",  -- Koby
-	["card_1100"] = "rbxassetid://0", -- Usopp
-	["card_1300"] = "rbxassetid://0", -- Chopper
-	["card_1400"] = "rbxassetid://0", -- Krillin
-	["card_1600"] = "rbxassetid://0", -- Yamcha
-	["card_1700"] = "rbxassetid://0", -- Midoriya
-	["card_1800"] = "rbxassetid://0", -- Piccolo
+	["card_600"] = "rbxassetid://130833058606753",  -- Tenten
+	["card_700"] = "rbxassetid://118833865011567",  -- Koby
+	["card_1100"] = "rbxassetid://85594190253475", -- Usopp
+	["card_1300"] = "rbxassetid://95964560683693", -- Chopper
+	["card_1400"] = "rbxassetid://88854946101555", -- Krillin
+	["card_1600"] = "rbxassetid://70847220149068", -- Yamcha
+	["card_1700"] = "rbxassetid://101838142917431", -- Midoriya
+	["card_1800"] = "rbxassetid://101009651230755", -- Piccolo
 }
 
 -- Class icons (TODO: Replace with actual class icons)
